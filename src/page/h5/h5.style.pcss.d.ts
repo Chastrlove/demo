477f1e1450bef9f35fc9e1da5208d1497e123f9f
zhoudonghua <donghuazh66@gmail.com>
2018-11-27 20:27:03 +0800

    no message

---
+++ src/page/h5/h5.style.pcss.d.ts
@@ -0,0 +1,4 @@
+export const app: string;
+export const appHeader: string;
+export const appTitle: string;
+export const appContent: string;
--- src/page/h5/h5.view.tsx
+++ src/page/h5/h5.view.tsx
@@ -6,14 +6,16 @@ import widgets from "../../common/silex-h5";
 
 import samples from "../../samples/index";
 
+import * as styles from './h5.style.pcss';
+
 class H5View extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to Silex-H5</h1>
+      <div className={styles.app}>
+        <header className={styles.appHeader}>
+          <h1 className={styles.appTitle}>Welcome to Silex-H5</h1>
         </header>
-        <div className="App-Content">
+        <div className={styles.appContent}>
           <Form {...samples.Widgets} widgets={widgets} />
         </div>
       </div>
